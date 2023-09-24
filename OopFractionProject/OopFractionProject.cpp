7f592578746df65b550a749c654dec7706b4aa54
maxefimov <efimov.top.academy@gmail.com>
2023-09-24 13:24:33 +0300

    Добавьте файлы проекта.

---
+++ OopFractionProject/OopFractionProject.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+class Fraction
+{
+    int numerator;
+    unsigned int denominator;
+public:
+    void SetNumerator(int n) { numerator = n; }
+    int GetNumerator() { return numerator; }
+    void SetDenominator(unsigned int d)
+    {
+        if (d == 0)
+            std::cout << "Error! Divid by zero!!!\n";
+        else
+            denominator = d;
+    }
+    int GetDenominator() { return denominator; }
+
+    void Print() 
+    {
+        std::cout << "[" << numerator << "/" << denominator << "]";
+    }
+};
+
+int main()
+{
+    Fraction f1;
+    f1.SetNumerator(5);
+    f1.SetDenominator(7);
+    f1.Print();
+}
